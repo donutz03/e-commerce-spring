7d8a992f9a7f79f96be64c5f3fefabd3d022bf5b
ionuthodoroaga <ionuthodoroaga@hotmail.com>
2023-08-12 13:35:32 +0300

    added exception handler

---
+++ src/main/java/com.donutz/ecommerce/exception/ApiExceptionHandler.java
@@ -0,0 +1,27 @@
+package main.java.com.donutz.ecommerce.exception;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+  
+  @SuppressWarnings("rawtypes")
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ErrorResponse> handle(ConstraintViolationException e) {
+    ErrorResponse errors = new ErrorResponse();
+    for (ConstraintViolation violation : e.getConstraintViolations()) {
+      ErrorItem error = new ErrorItem();
+      error.setCode(violation.getMessageTemplate());
+      error.setMessage(violation.getMessage());
+      errors.addError(error);
+    }
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @SuppressWarnings("rawtypes")
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<ErrorItem> handle(ResourceNotFoundException e) {
+    ErrorItem error = new ErrorItem();
+    error.setMessage(e.getMessage());
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+}
